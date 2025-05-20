d9fb3f1fb436f2b564c904c9c1f3bac0d19008d7
try-max <52982945+try-max@users.noreply.github.com>
2025-05-21 01:07:19 +0800

    Update dict.ts

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -1,17 +1,51 @@
 // ? 系统全局字典
 
 /**
- * @description：用户性别
+ * @description：请求方式
  */
 export const genderType = [
-  { label: "男", value: 1 },
-  { label: "女", value: 2 }
+  { label: "GET", value: "GET" },
+  { label: "POST", value: "POST" },
+  { label: "PUT", value: "PUT" },
+  { label: "DELETE", value: "DELETE" }
 ];
 
+/**
+ *
+ *   用例执行环境
+ */
+
+export const caseType = [
+  { label: "测试环境", value: "rc" },
+  { label: "stage环境", value: "stage" },
+  { label: "生产环境", value: "prod" }
+];
+
+/**
+ *
+ *   用例模块
+ */
+
+export const moduleType = [
+  { label: "数据中心", value: "数据中心" },
+  { label: "BI", value: "BI" },
+  { label: "CRM", value: "CRM" },
+  { label: "CSDN", value: "CSDN" }
+];
 /**
  * @description：用户状态
  */
 export const userStatus = [
   { label: "启用", value: 1, tagType: "success" },
   { label: "禁用", value: 0, tagType: "danger" }
+];
+
+/**
+ *
+ *   用例状态
+ */
+
+export const caseStatues = [
+  { label: "启用", value: 1 },
+  { label: "废弃", value: -1 }
 ];
