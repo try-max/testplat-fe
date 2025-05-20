992c36fa93ba75a38e979b32ee39ea4832f7922a
try-max <52982945+try-max@users.noreply.github.com>
2025-05-21 01:06:18 +0800

    Update index.ts

--- src/stores/interface/index.ts
+++ src/stores/interface/index.ts
@@ -31,6 +31,10 @@ export interface UserState {
   userInfo: { name: string };
 }
 
+export interface UserMessage {
+  userName: string;
+}
+
 /* tabsMenuProps */
 export interface TabsMenuProps {
   icon: string;
